d3a3631645832c12158f1af7c8a8a94c95c88142
Onekru <gmodkru@gmail.com>
2024-09-04 13:28:08 +0300

    Initial commit

---
+++ src/main/java/javaqa/Post.java
@@ -0,0 +1,10 @@
+package javaqa;
+
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+}
